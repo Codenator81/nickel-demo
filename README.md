c7a390c42d0c0e798aa58879ec7422a1867e9afe
Codenator81 <codenator81@gmail.com>
2015-04-30 22:14:48 +0300

    First Step cargo run locate to localhost:8080/

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # nickel-demo
 Rust web lessons
-All info on WIKI page
+Just follow my commits 
+I try do every commits step by step where each compile
+
+This tutorial follow Rust beta release
+
+First Step : I create minimal server
+When do "cargo run" locate to http://localhost:8080/
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use nickel::Nickel;
 
 fn main() {
     let mut server = Nickel::new();
-
+    // print in browser hello world in any location after /
+    // for example http://localhost:8080/foo/bar
     server.utilize(router! {
         get "**" => |_req, _res| {
         "Hello world!"
